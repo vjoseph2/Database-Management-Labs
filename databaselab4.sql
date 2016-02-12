2e113869da967d4084dbb7fb096fbb1e198a0fa9
vjoseph2 <valliejoseph2@gmail.com>
2016-02-11 19:25:04 -0500

    Starting lab from in class examples

---
+++ databaselab4.sql
@@ -0,0 +1,12 @@
+﻿--Lab 4--
+
+/* Get the cities of agents 
+booking an order for a customer whose cid is 'c002'. */
+
+SELECT city
+FROM agents WHERE aid in(
+			SELECT aid
+			FROM orders WHERE cid ='c002'
+			);
+
+SELECT * FROM agents;
