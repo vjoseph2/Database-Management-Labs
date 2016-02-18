e8ea8fa1c108a7268e0c3c1c35a496dcba1f031c
vjoseph2 <valliejoseph2@gmail.com>
2016-02-17 23:15:14 -0500

    Created new Lab4 directory, uploaded lab 4 sql document

---
+++ Lab4/databaselab4-2.sql
@@ -0,0 +1,42 @@
+﻿--Lab 4--
+--Database Management--
+
+--Get the cities of agents booking an order for a customer whose cid is ‘c002’.--
+SELECT city FROM agents WHERE aid in(
+			SELECT aid
+			FROM orders WHERE cid ='c002'
+			);
+--Get the ids of products ordered through any agent who takes at least one order from a customer in Dallas, sorted by pid from highest to lowest.--
+SELECT pid FROM orders WHERE aid in (
+			SELECT aid 
+			from agents WHERE city ='Dallas'
+			) ORDER BY pid ASC;
+
+--Get the ids and names of customers who did not place an order through agent a01.--
+SELECT cid, name  from customers WHERE cid in(
+			SELECT cid 
+			FROM orders WHERE aid NOT IN ('a01')
+			);
+--Get the ids of customers who ordered both product p01 and p07.--
+SELECT DISTINCT cid from orders WHERE cid in (
+			SELECT cid 
+			from orders WHERE pid='p01' AND cid in (
+								SELECT cid 
+								FROM orders WHERE pid='p07')
+						);
+--Get the ids of products not ordered by any customers who placed any order through agent a07 in pid order from highest to lowest.-
+SELECT DISTINCT pid FROM orders WHERE cid in (
+			SELECT cid 
+			FROM orders WHERE aid NOT IN ('a07')
+			) ORDER BY pid DESC;
+--Get the name, discounts, and city for all customers who place orders through agents in London or New York.--
+SELECT name, city, discount FROM customers WHERE cid in (
+							SELECT cid 
+							FROM orders WHERE aid in (
+										SELECT aid 
+										FROM agents WHERE city= 'London' or city ='New York')
+							);
+--Get all customers who have the same discount as that of any customers in Dallas or London--
+SELECT name FROM customers WHERE discount in (
+											SELECT discount 
+											FROM customers WHERE city	in ('London','Dallas'));
--- databaselab4.sql
+++ databaselab4.sql
@@ -9,4 +9,49 @@ FROM agents WHERE aid in(
 			FROM orders WHERE cid ='c002'
 			);
 
-SELECT * FROM agents;
+SELECT name 
+FROM agents
+WHERE  aid in ( SELECT aid 
+		FROM orders 
+		WHERE pid in (SELECT pid 
+				FROM products where name='pencil'
+				)
+		);
+
+--Shows only unique elements--
+SELECT distinct mon
+FROM orders ;
+
+--Order By--
+SELECT distinct mon
+FROM orders 
+ORDER by mon DESC/ASC;
+
+--Maximum and Minimum--
+SELECT min(dollars)/max
+FROM orders;
+
+
+SELECT max(mon)
+FROM orders;
+
+SELECT sum(dollars)
+FROM orders;
+
+SELECT AVG(dollars)
+FROM orders;
+
+SELECT* 
+FROM orders WHERE dollars > (SELECT AVG(dollars) FROM orders);
+
+--union--
+SELECT count(*)
+WHERE city in ( SELECT city 
+FROM customers union all
+SELECT city
+FROM agents);
+
+-- what is the total of orders by month--
+SELECT sum(dollars)
+FROM orders
+WHERE mon='jan'
