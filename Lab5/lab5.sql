fa259019877e1313d485c369b9c27542e9550ae9
vjoseph2 <valliejoseph2@gmail.com>
2016-02-24 15:22:21 -0500

    Added #2

--- Lab5/lab5.sql
+++ Lab5/lab5.sql
@@ -2,5 +2,6 @@
 SELECT city FROM agents a, orders o WHERE a.aid=o.aid and o.cid='c002'
 --2.Show the ids of products ordered through any agent who makes at least one order for a customer in--
 --Dallas, sorted by pid from highest to lowest. Use joins; no subqueries.--
-
 SELECT pid from customers c, orders o WHERE c.cid=o.cid AND c.city='Dallas'
+--3.Show the names of customers who have never placed an order. Use a subquery.--
+SELECT name FROM customers WHERE cid in (SELECT cid FROM orders WHERE cid NOT IN(SELECT cid FROM orders));
