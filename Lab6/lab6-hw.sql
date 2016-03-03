f8e764d9c9d8eb8bc8306b838f70427307294f87
vjoseph2 <valliejoseph2@gmail.com>
2016-03-02 20:39:16 -0500

    Added #3

--- Lab6/lab6-hw.sql
+++ Lab6/lab6-hw.sql
@@ -8,4 +8,12 @@
 
 SELECT name, city FROM customers WHERE city in (SELECT city from products WHERE priceusd > (SELECT AVG(priceusd) from products)) 
 
---2
+--2 Display the names of products whose priceUSD is strictly above the average priceUSD, in reversealphabetical
+--order.
+
+SELECT name, priceusd from products WHERE priceUSD > (SELECT avg(products.priceUSD)FROM products) ;
+
+
+--3.Display the customer name, pid ordered, and the total for all orders, sorted by total from high to low--
+SELECT customers.name, o.pid, o.dollars FROM orders o INNER JOIN customers ON o.cid = customers.cid ORDER BY o.dollars DESC;
+
